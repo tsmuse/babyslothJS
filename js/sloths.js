0389752192d95aa004fd73f94294372f69e4d5d8
tsmuse <tony@notjustprettypictures.com>
2016-05-14 15:16:11 -0700

    moved sloth serving to a function. Added fallback to local ver of json if remote ver can't be loaded

--- js/sloths.js
+++ js/sloths.js
@@ -5,19 +5,24 @@ $.getJSON(slothpen)
 .done(function(data){
 	console.log("finished");
 	sloths = data;
-	pick = Math.floor(Math.random() * (sloths.length +1));
-	sloth = sloths[pick];
-	// inserting the correct image and data goes here. 
-	$('.image-area').html("<img src='" + sloth.image_url + "' alt='baby sloth image from " + sloth.image_source + "'/>");
-	$('.credits-area').html("<p>Original: <a href='" + sloth.image_source + "'>" + sloth.title + "</a></p>");
-	//$('body').append("<p><b>" + sloth.title + "</b></p><p>" + sloth.image_url + "</p><p><i>" + sloth.image_source + "</i></p>");
+	slothme();
+})
+.fail(function(){
+	console.log("Couldn't find sloths in GitHub. Github may be down?");
+	console.log("pulling sloths from local source. May not be as fresh.");
+	$.getJSON("sloths.json")
+	.done(function(data){
+		console.log("finished from local");
+		sloths = data;
+		slothme();
+	});
 });
 
 $('.sloth-button').on('click',function(event){
 	event.preventDefault();
 	slothme();
 });
-//function to handle refresh
+//function to handle putting baby sloths in ya face
 function slothme(){
 	pick = Math.floor(Math.random() * (sloths.length +1));
 	sloth = sloths[pick];
